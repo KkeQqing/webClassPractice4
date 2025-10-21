3ae0b40d646f1a7e5b1dd17cf2a15928b7180511
kq <1798178975@qq.com>
2025-10-21 20:53:40 +0800

    .

--- popup.js
+++ popup.js
@@ -2,3 +2,9 @@ document.getElementById('help-btn').addEventListener('click', () => {
   const url = chrome.runtime.getURL('help.html');
   window.open(url, '_blank');
 });
+
+document.getElementById('chose-btn').addEventListener('click', () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { action: "showAreaSelector" });
+  });
+});
